8bc65fab31663984fa7e46504cba74340a6ce392
Egemen Noyan Seçkin <egemennoyan@hotmail.com>
2024-07-12 20:46:07 +0300

    RF-2 improve code in Account and AccountCard

--- frontend/src/components/AccountCard/AccountCard.js
+++ frontend/src/components/AccountCard/AccountCard.js
@@ -1,237 +1,179 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
+import { Box, Typography, Button } from '@mui/material';
+import axios from 'axios';
+import { styles } from './StylesAccountCard';
+import UserContext from 'contexts/UserContext';
 
-import { Box, Typography, Button } from '@mui/material'
-import axios from 'axios'
-import { useWindowSize } from "@react-hook/window-size/throttled";
+export const AccountCard = ({ activePage, setItemInfos, setIsAcceptOrReject, setIsBuyModal, offerInfos }) => {
+  const { userAuth } = useContext(UserContext);
 
-import { styles } from './StylesAccountCard'
-import UserContext from '../../contexts/UserContext'
-import MobileContext from '../../contexts/MobileContext'
+  // update users recieved offers
+  async function updateOfferStatus(bool) {
+    const id = offerInfos.offerId;
+    const address = `https://bootcamp.akbolat.net/offers/${id}`;
 
-export default function CardItem({ activePage, setItemInfos, setIsAcceptOrReject, setIsBuyModal, offerInfos }) {
-
-    const { userAuth } = useContext(UserContext);
-    const mobileScreen = useContext(MobileContext)
-    const [width] = useWindowSize({ fps: 60 });
-    
-    
-    // update users recieved offers
-    async function updateOfferStatus(bool) {
-
-        const id = offerInfos.offerId;
-        const address = `https://bootcamp.akbolat.net/offers/${id}`
-
-        await axios.put(
-            address, 
-            { 
-                isStatus: bool 
-            }, 
-            {
-            headers: {
-                Authorization: `Bearer ${userAuth.token}`
-            }
-        })
-        .then(response => {
-            setItemInfos(prevInfos => ({ ...prevInfos, itemId: response.data.product.id, offerId: response.data.id }))
-        })
-        .catch(err => console.log(err))
-
-    }
-
-    const handlePurchase = () => {
-        setIsBuyModal(true);
-        setItemInfos(prevInfos => ({...prevInfos, boughtProductId: offerInfos.productId}))
-
-    }
-
-    const renderCardName = () => {
-        let cardName = offerInfos.name;
-
-        // if name lenght is long, add ... dots after 23 chars
-        if (width < mobileScreen && cardName) {
-            const totalWhitespace = cardName.split(' ').length - 1;
-            const totalCharacters = cardName.length;
-            const totalSpace = totalWhitespace + totalCharacters;
-            if (totalSpace > 24) {
-                cardName = cardName.substring(0, 23) + '...';
-            }
+    await axios
+      .put(
+        address,
+        {
+          isStatus: bool,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${userAuth.token}`,
+          },
         }
+      )
+      .then((response) => {
+        setItemInfos((prevInfos) => ({ ...prevInfos, itemId: response.data.product.id, offerId: response.data.id }));
+      })
+      .catch((err) => console.log(err));
+  }
 
-        return (
-            <Typography variant='h6'>
-                {cardName}
-            </Typography>
-        )
+  const handlePurchase = () => {
+    setIsBuyModal(true);
+    setItemInfos((prevInfos) => ({ ...prevInfos, boughtProductId: offerInfos.productId }));
+  };
+  const checkOfferStatus = () => {
+    const offerStatus = offerInfos.status;
+    let text = '';
+    let textColor = '';
+
+    // if offer approved
+    if (offerStatus) {
+      text = 'Onaylandı';
+      textColor = 'primary.main';
+    }
+    // if offer not responded yet
+    else if (offerStatus === null) {
+      text = 'Beklemede';
+      textColor = 'orange';
+    }
+    // if offer rejected
+    else if (!offerStatus) {
+      text = 'Reddedildi';
+      textColor = 'danger';
     }
 
-    const renderCardImage = () => {
+    return { text, textColor };
+  };
 
-        return (
-            <img src={offerInfos.image} alt=""
-                style={styles.image} />
-        )
-    }
-
-    const renderPrice = () => {
-
-        const price = parseFloat(offerInfos.offerPrice).toFixed(2);
-
-        return (
-            <>
-                <Typography sx={{
-                    ...styles.priceText,
-                    color: '#B1B1B1',
-                }}>
-                    AIınan Teklif:
-                </Typography>
-                <Typography sx={{
-                    ...styles.priceText,
-                    fontWeight: '700',
-                }}>
-                    {price} TL
-                </Typography>
-            </>
-        )
-    }
-
-    function renderCardNameImagePrice() {
-
-        const imageView = renderCardImage();
-        const nameView = renderCardName();
-        const priceView = renderPrice();
-
-        return (
-            <Box sx={{ display: 'flex', gap: 1.5 }}>
-                {imageView}
-                <Box>
-                    {nameView}
-                    <Box sx={styles.priceContainer}>
-                        {priceView}
-                    </Box>
-                </Box>
-            </Box>
-        )
-    }
-
-    const checkOfferStatus = () => {
-
-        const offerStatus = offerInfos.status;
-        let text = '';
-        let textColor = '';
-
-        // if offer approved
-        if (offerStatus) {
-            text = 'Onaylandı';
-            textColor = 'primary.main';
-        }
-        // if offer not responded yet
-        else if (offerStatus === null) {
-            text = 'Beklemede';
-            textColor = 'orange';
-        }
-        // if offer rejected
-        else if (!offerStatus) {
-            text = 'Reddedildi';
-            textColor = 'danger';
-        }
-
-        return { text, textColor };
-    }
-
-    const renderOfferStatus = () => {
-
-        const { text, textColor } = checkOfferStatus();
-
-        return (
-            <Typography variant='h6'
-                sx={{ ...styles.offerStatus, color: textColor }}>
-                {text}
-            </Typography>
-        )
-    }
-
-    const handleApprove = () => {
-        setIsAcceptOrReject(true);
-        updateOfferStatus(true);
-    }
-
-    const handleReject = () => {
-        setIsAcceptOrReject(false);
-        updateOfferStatus(false);
-    }
-
-    function renderRecievedOfferBtnsAndText() {
-
-        const status = offerInfos.status;
-
-        if (status === null) {
-
-            return (
-                <Box sx={{
-                    alignSelf: { xs: 'end', lg: 'center' }
-                }}>
-                    <Button variant='contained'
-                        onClick={handleApprove}
-                        sx={{
-                            ...styles.approveButton,
-                            px: { xs: 3.85 }
-                        }}>
-                        Onayla
-                    </Button>
-
-                    <Button onClick={handleReject}
-                        variant='contained'
-                        sx={styles.rejectButton}>Reddet
-                    </Button>
-                </Box>
-            )
-        }
-        else {
-            const statusView = renderOfferStatus();
-            return statusView;
-        }
-
-    }
-
-    function renderSentOffersBtnAndStatus() {
-
-        const status = offerInfos.status;
-        const isSold = offerInfos.isSold;
-
-        let buttonView;
-        const statusView = renderOfferStatus();
-
-        if (status && !isSold) {
-            buttonView = (
-                <Button variant='contained'
-                    onClick={handlePurchase}
-                    sx={{
-                        ...styles.approveButton,
-                        mr: 3.5
-                    }}>
-                    Satın AI
-                </Button>)
-        }
-        return (
-            <Box sx={{
-                alignSelf: { xs: 'end', lg: 'center' },
-                display: 'flex',
-            }}>
-                {buttonView}
-                {statusView}
-            </Box>
-        )
-
-    }
-
-    const cardNameImagePriceView = renderCardNameImagePrice();
-
-    const buttonsView = activePage === 'Teklif Aldıklarım' ? renderRecievedOfferBtnsAndText() : renderSentOffersBtnAndStatus();
+  const renderOfferStatus = () => {
+    const { text, textColor } = checkOfferStatus();
 
     return (
-        <Box sx={styles.offerContainer}>
-            {cardNameImagePriceView}
-            {buttonsView}
+      <Typography variant="h6" sx={{ ...styles.offerStatus, color: textColor }}>
+        {text}
+      </Typography>
+    );
+  };
+
+  const handleApprove = () => {
+    setIsAcceptOrReject(true);
+    updateOfferStatus(true);
+  };
+
+  const handleReject = () => {
+    setIsAcceptOrReject(false);
+    updateOfferStatus(false);
+  };
+
+  function renderRecievedOfferBtnsAndText() {
+    const status = offerInfos.status;
+
+    if (status === null) {
+      return (
+        <Box
+          sx={{
+            alignSelf: { xs: 'end', lg: 'center' },
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={handleApprove}
+            sx={{
+              ...styles.approveButton,
+              px: { xs: 3.85 },
+            }}
+          >
+            Onayla
+          </Button>
+
+          <Button onClick={handleReject} variant="contained" sx={styles.rejectButton}>
+            Reddet
+          </Button>
         </Box>
-    )
-}
+      );
+    } else {
+      const statusView = renderOfferStatus();
+      return statusView;
+    }
+  }
+
+  function renderSentOffersBtnAndStatus() {
+    const status = offerInfos.status;
+    const isSold = offerInfos.isSold;
+
+    let buttonView;
+    const statusView = renderOfferStatus();
+
+    if (status && !isSold) {
+      buttonView = (
+        <Button
+          variant="contained"
+          onClick={handlePurchase}
+          sx={{
+            ...styles.approveButton,
+            mr: 3.5,
+          }}
+        >
+          Satın AI
+        </Button>
+      );
+    }
+    return (
+      <Box
+        sx={{
+          alignSelf: { xs: 'end', lg: 'center' },
+          display: 'flex',
+        }}
+      >
+        {buttonView}
+        {statusView}
+      </Box>
+    );
+  }
+
+  const buttonsView =
+    activePage === 'Teklif Aldıklarım' ? renderRecievedOfferBtnsAndText() : renderSentOffersBtnAndStatus();
+
+  return (
+    <Box sx={styles.offerContainer}>
+      <Box sx={{ display: 'flex', gap: 1.5 }}>
+        <img src={offerInfos.image} alt="" style={styles.image} />
+        <Box>
+          <Typography variant="h6">{offerInfos.name}</Typography>
+          <Box sx={styles.priceContainer}>
+            <Typography
+              sx={{
+                ...styles.priceText,
+                color: '#B1B1B1',
+              }}
+            >
+              AIınan Teklif:
+            </Typography>
+            <Typography
+              sx={{
+                ...styles.priceText,
+                fontWeight: '700',
+              }}
+            >
+              {parseFloat(offerInfos.offerPrice).toFixed(2)} TL
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+      {buttonsView}
+    </Box>
+  );
+};
--- frontend/src/pages/Account/Account.js
+++ frontend/src/pages/Account/Account.js
@@ -1,266 +1,57 @@
-import React, { useState, useContext, useEffect } from 'react'
-
-import { Container, Box, Typography } from '@mui/material'
-import axios from 'axios'
-import { useWindowSize } from "@react-hook/window-size/throttled";
-
-import UserContext from '../../contexts/UserContext.js'
-import MobileContext from "../../contexts/MobileContext.js";
-
+import React, { useState, useContext } from 'react';
+import { Container, Box, Typography } from '@mui/material';
+import UserContext from '../../contexts/UserContext.js';
 import { styles } from './StylesAccount.js';
-import AccountCard from '../../components/AccountCard/AccountCard.js'
-import AccountIcon from '../../constants/icons/AccountIcon.js'
-import BuyModal from '../../components/BuyModal/BuyModal.js'
-import Alert from '../../components/Alert/Alert.js'
-
-export default function Account() {
-
-    const { userAuth } = useContext(UserContext);
-    const [width] = useWindowSize({ fps: 60 });
-    const mobileScreen = useContext(MobileContext);
-
-    const [activePage, setActivePage] = useState('Teklif Aldıklarım');
-
-    const [recievedOffers, setRecievedOffers] = useState([]);
-
-    const [sentOffers, setSentOffers] = useState([]);
-
-    // used for getting product and offer ids 
-    const [itemInfos, setItemInfos] = useState({});
-
-    const [isAcceptOrReject, setIsAcceptOrReject] = useState(false);
-    const [isProductBought, setIsProductBought] = useState(false);
-
-    const [isBuyModal, setIsBuyModal] = useState(false);
-
-    // fetch data 
-    useEffect(() => {
-
-        if (activePage === 'Teklif Aldıklarım') {
-
-            async function fetchRecievedOffers() {
-
-                const address = `https://bootcamp.akbolat.net/products?users_permissions_user=${userAuth.id}`;
-
-                axios.get(
-                    address,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${userAuth.token}`
-                        }
-                    })
-                    .then(response => setRecievedOffers(response.data))
-                    .catch(err => console.log(err))
-            }
-
-            fetchRecievedOffers();
-        }
-        else if (activePage === 'Teklif Verdiklerim') {
-
-            async function fetchSentOffers() {
-
-                const address = `https://bootcamp.akbolat.net/offers?users_permissions_user=${userAuth.id}`;
-
-                axios.get(
-                    address,
-                    {
-                        headers: {
-                            Authorization: `Bearer ${userAuth.token}`
-                        }
-                    }
-                )
-                    .then(response => setSentOffers(response.data))
-                    .catch(err => console.log(err))
-            }
-
-            fetchSentOffers();
-        }
-
-    }, [activePage, userAuth.id, userAuth.token])
-
-    // update recievedOffers on change to avoid api request
-    useEffect(() => {
-
-        if (itemInfos.offerId) {
-
-            const filteredArray = [];
-
-            recievedOffers.forEach(product => {
-
-                if (product.id === itemInfos.itemId) {
-
-                    product.offers.forEach(offer => {
-                        if (offer.id === itemInfos.offerId) {
-                            offer.isStatus = isAcceptOrReject;
-                            filteredArray.push(product);
-                        }
-                    })
-                } else filteredArray.push(product);
-            })
-            setRecievedOffers(filteredArray);
-        }
-    }, [itemInfos.offerId])
-
-    //  update sentOffers array to avoid api request
-    useEffect(() => {
-
-        if (isProductBought) {
-
-            let filteredArray = [];
-            sentOffers.forEach(offer => {
-
-                if (offer?.product?.id === itemInfos.boughtProductId) {
-                    offer.product.isSold = true;
-                    filteredArray.push(offer);
-                }
-                else filteredArray.push(offer);
-            })
-            setSentOffers(filteredArray);
-        }
-    }, [isProductBought])
-
-
-
-    function renderRecievedOffers() {
-
-        const cards = recievedOffers?.map(product => (
-
-            product.offers?.map((offer, index) => {
-                const imageUrl = width > mobileScreen ? product.image?.url : product?.image?.formats?.thumbnail?.url;
-
-                const image = `https://bootcamp.akbolat.net${imageUrl}`;
-
-                const offerInfos = {
-                    name: product.name,
-                    image,
-                    offerPrice: offer?.offerPrice,
-                    offerId: offer?.id,
-                    status: offer?.isStatus,
-                }
-                return (
-                    <AccountCard key={index} offerInfos={offerInfos}
-                        setItemInfos={setItemInfos}
-                        activePage={activePage}
-                        setIsAcceptOrReject={setIsAcceptOrReject}
-                    />
-                )
-            })
-        ))
-
-        return cards;
-    }
-
-
-
-    const renderSentOffers = () => {
-
-        const accountCards = sentOffers.map((offer, index) => {
-
-            const offerInfos = {
-                name: offer?.product?.name,
-                image: `https://bootcamp.akbolat.net${offer?.product?.image?.url}`,
-                offerPrice: offer?.offerPrice,
-                status: offer?.isStatus,
-                productId: offer?.product?.id,
-                isSold: offer?.product?.isSold,
-            }
-
-            return (
-                <AccountCard key={index} offerInfos={offerInfos} activePage={activePage}
-                    setIsBuyModal={setIsBuyModal}
-                    setItemInfos={setItemInfos} />
-            )
-        })
-
-        return accountCards;
-    }
-
-    function renderEmail(email) {
-
-        return (
-            <Box sx={styles.emailContainer}>
-                <AccountIcon />
-                <Typography sx={styles.emailText}>
-                    {email}
-                </Typography>
-            </Box>
-        )
-    }
-
-    function renderNavs() {
-
-        const navs = ['Teklif Aldıklarım', 'Teklif Verdiklerim']
-
-        const navEls = navs.map(nav => {
-            return (
-                <Typography key={nav} className={nav === activePage && 'active-nav'} onClick={() => setActivePage(nav)}
-                    sx={{
-                        ...styles.navText,
-                        fontWeight: nav === activePage && '700',
-                    }}>
-                    {nav}
-                </Typography>
-            )
-        })
-
-        return (
-            <Box sx={styles.navContainer}>
-                {navEls}
-                <hr className='account-hr' />
-            </Box>
-        )
-    }
-
-    function renderPage() {
-
-        const pageView = activePage === 'Teklif Aldıklarım' ? renderRecievedOffers() : renderSentOffers()
-
-        return (
-            <Box sx={styles.columnReverse}>
-                {pageView}
-            </Box>
-        )
-    }
-
-    const renderBuyModal = () => {
-
-        return (
-            <BuyModal isBuyModal={isBuyModal} setIsBuyModal={setIsBuyModal} productId={itemInfos?.boughtProductId} setIsProductBought={setIsProductBought}
-                token={userAuth.token} />
-        )
-    }
-
-    const renderAlert = () => {
-
-        return (
-            <Alert isAlert={isProductBought} setIsAlert={setIsProductBought} alertText='Satın Alındı' />
-        )
-    }
-
-    const emailView = renderEmail(userAuth.email);
-    const navsView = renderNavs();
-    const pageView = renderPage();
-    const buyModalView = renderBuyModal();
-    const alertView = renderAlert();
-
-    return (
-        <Box sx={styles.boxContainer}>
-
-            <Container maxWidth="xl"
-                sx={styles.container}>
-
-                {emailView}
-
-                <Box sx={styles.pageContainer}>
-                    {navsView}
-                    {pageView}
-                </Box>
-
-            </Container>
-
-            {buyModalView}
-            {alertView}
-
+import AccountIcon from '../../constants/icons/AccountIcon.js';
+import { BuyModal } from 'components/BuyModal/BuyModal.js';
+import Alert from '../../components/Alert/Alert.js';
+
+const Account = () => {
+  const { userAuth } = useContext(UserContext);
+  const navs = ['Ürünlerim', 'Teklif Aldıklarım', 'Teklif Verdiklerim'];
+
+  const [activePage, setActivePage] = useState('Teklif Aldıklarım');
+  const [isProductBought, setIsProductBought] = useState(false);
+  const [isBuyModalOpen, setIsBuyModalOpen] = useState(false);
+
+  return (
+    <Box sx={styles.boxContainer}>
+      <Container maxWidth="xl" sx={styles.container}>
+        <Box sx={styles.emailContainer}>
+          <AccountIcon />
+          <Typography sx={styles.emailText}>{userAuth.email}</Typography>
         </Box>
-    )
-}
+
+        <Box sx={styles.pageContainer}>
+          <Box sx={styles.navContainer}>
+            {navs.map((nav) => (
+              <Typography
+                key={nav}
+                className={nav === activePage && 'active-nav'}
+                onClick={() => setActivePage(nav)}
+                sx={{
+                  ...styles.navText,
+                  fontWeight: nav === activePage && '700',
+                }}
+              >
+                {nav}
+              </Typography>
+            ))}
+            <hr className="account-hr" />
+          </Box>
+        </Box>
+      </Container>
+
+      <BuyModal
+        isOpen={isBuyModalOpen}
+        onClose={() => setIsBuyModalOpen(false)}
+        // TODO: Handle buy product
+        onBuyProduct={() => {}}
+      />
+
+      <Alert isAlert={isProductBought} setIsAlert={setIsProductBought} alertText="Satın Alındı" />
+    </Box>
+  );
+};
+
+export default Account;
