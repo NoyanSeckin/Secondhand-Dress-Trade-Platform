5efaa81b653c3bf98229de531a04c3d41b39cad0
Egemen Noyan Seçkin <egemennoyan@hotmail.com>
2024-07-12 21:09:30 +0300

    RF-2 improve code in Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
-import { Routes, Route,} from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
-import {ThemeProvider} from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 
-import './styles.css'
-import  {globalTheme}  from './theme/GlobalTheme';
-import UserToken from './contexts/UserContext'
-import ProductContext from "./contexts/ProductContext";
-import Home from './pages/Home/Home'
-import Authentication from './pages/Authentication/Authentication'
-import Detail from './pages/Detail/Detail'
-import Account from './pages/Account/Account'
-import AddProduct from "./pages/AddProduct/AddProduct";
-import ProtectedRoutes from './routes/ProtectedRoutes'
-import MobileContext from "./contexts/MobileContext";
-import Navbar from "./components/Navbar/Navbar";
+import './styles.css';
+import { globalTheme } from './theme/GlobalTheme';
+import UserToken from './contexts/UserContext';
+import ProductContext from './contexts/ProductContext';
+import Home from './pages/Home/Home';
+import Authentication from './pages/Authentication/Authentication';
+import Detail from './pages/Detail/Detail';
+import Account from './pages/Account/Account';
+import AddProduct from './pages/AddProduct/AddProduct';
+import ProtectedRoutes from './routes/ProtectedRoutes';
+import MobileContext from './contexts/MobileContext';
+import { Navbar } from 'components/Navbar/Navbar';
 
 function App() {
   let location = useLocation();
@@ -23,61 +23,56 @@ function App() {
   const [userAuth, setUserAuth] = useState({});
   const [product, setProduct] = useState([]);
 
-  useEffect(()=> {
-    function setAuthStateAfterRefresh(){
+  useEffect(() => {
+    function setAuthStateAfterRefresh() {
+      const initialValue = {};
+      const userInfo = document.cookie
+        .split(';')
+        .map((cookie) => cookie.split('='))
+        .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: value }), initialValue);
 
-        const initialValue = {};
-        const userInfo = document.cookie.split(';').map(cookie => cookie.split('=')).reduce((accumulator, [key, value]) => ({...accumulator, [key.trim()]: value}), initialValue);
-        
-        setUserAuth({email: userInfo.email, token: userInfo.token, id: userInfo.id});
+      setUserAuth({ email: userInfo.email, token: userInfo.token, id: userInfo.id });
     }
 
-      setAuthStateAfterRefresh();
-  }, [])
-  
-  function renderIndexElement(){
-    return (
-        userAuth.token ? 
-        <Home/> 
-        : 
-         <Authentication/>
-    )
+    setAuthStateAfterRefresh();
+  }, []);
+
+  function renderIndexElement() {
+    return userAuth.token ? <Home /> : <Authentication />;
   }
 
   // display navbar except auth page
-  function renderNavbar(){
-
-      if(location.pathname !== '/auth'){
-        return <Navbar/>
-      }
-
+  function renderNavbar() {
+    if (location.pathname !== '/auth') {
+      return <Navbar />;
+    }
   }
 
   const navbarView = renderNavbar();
   const indexElement = renderIndexElement();
-  
+
   return (
-  <div className="App">
-    <MobileContext.Provider value={400}>
-    <UserToken.Provider value={{userAuth, setUserAuth}}>
-     <ProductContext.Provider value={{product, setProduct}}>
-       <ThemeProvider theme={globalTheme}>
-            {navbarView}
-            <Routes>
-              <Route  path="/" element={indexElement}/> 
-              <Route index path="/home" element={<Home/>}/>
-              <Route path="*" element={<Home/>}/>
-              <Route path="/detail" element={<Detail/>}/>
-              <Route element={<ProtectedRoutes/>}>
-                <Route path="/account" element={<Account/>}/>
-                <Route path="/addproduct" element={<AddProduct/>}/>
-              </Route>  
-            </Routes>
-      </ThemeProvider>
-      </ProductContext.Provider> 
-    </UserToken.Provider>
-    </MobileContext.Provider>
-  </div>
+    <div className="App">
+      <MobileContext.Provider value={400}>
+        <UserToken.Provider value={{ userAuth, setUserAuth }}>
+          <ProductContext.Provider value={{ product, setProduct }}>
+            <ThemeProvider theme={globalTheme}>
+              {navbarView}
+              <Routes>
+                <Route path="/" element={indexElement} />
+                <Route index path="/home" element={<Home />} />
+                <Route path="*" element={<Home />} />
+                <Route path="/detail" element={<Detail />} />
+                <Route element={<ProtectedRoutes />}>
+                  <Route path="/account" element={<Account />} />
+                  <Route path="/addproduct" element={<AddProduct />} />
+                </Route>
+              </Routes>
+            </ThemeProvider>
+          </ProductContext.Provider>
+        </UserToken.Provider>
+      </MobileContext.Provider>
+    </div>
   );
 }
 
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,90 +1,34 @@
-import { AppBar, Container, Button, Box, Typography } from '@mui/material'
-import { Link } from 'react-router-dom'
-import React from 'react'
-import Logo from '../../constants/icons/Logo.js'
-import PlusIcon from '../../constants/icons/PlusIcon.js'
-import PersonIcon from '../../constants/icons/PersonIcon.js'
-import { styles } from './StylesNavbar.js'
+import { AppBar, Container, Button, Box, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Logo from 'constants/icons/Logo.js';
+import PlusIcon from 'constants/icons/PlusIcon.js';
+import PersonIcon from 'constants/icons/PersonIcon.js';
+import { styles } from './StylesNavbar.js';
 
-export default function Navbar() {
-
-  function renderAccountBtn() {
-
-    const signIn = {
-      text: 'Giriş Yap',
-      path: '/',
-    }
-  
-    const account = {
-      text: 'Hesabım',
-      path: '/account'
-    }
-
-    let buttonLabel;
-    let path;
-    const isUserAuth = Boolean(document.cookie);
-
-      if (isUserAuth) {
-          buttonLabel = account.text;
-          path = account.path;
-        } 
-        else {
-          buttonLabel = signIn.text;
-          path = signIn.path;
-        }
-
-      return (
-        <Link to={path}>
-          <Button sx={styles.btn}><PersonIcon />{buttonLabel}</Button>
-        </Link>
-      )
-      
-  }
-  
-  function renderAddProductBtn() {
-
-      return (
-        <Link to="/addproduct">
-            <Button sx={styles.btn}>
-                <PlusIcon sx={styles.plusIcon}/>
-                <Typography sx={styles.addProductText}>
-                    Ürün Ekle
-                </Typography>
-            </Button>
-          </Link>
-      )
-  }
-
-  function  renderLogo() {
-    
-    return(
-      <Link to="/home">
-          <Logo sx={styles.logo} />
-       </Link>
-    )
-
-  }
-
-  const renderBtns = () => {
-
-    return(
-      <Box sx={styles.btnsContainer}>
-          {renderAddProductBtn()}
-          {renderAccountBtn()}
-       </Box>
-    )
-
-  }
-
-  const logoView = renderLogo();
-  const btnsView = renderBtns();
-  
+export const Navbar = () => {
   return (
     <AppBar elevation={0} sx={styles.appbar}>
-        <Container maxWidth='xl' sx={styles.container}>
-            {logoView}
-            {btnsView}
-        </Container>
+      <Container maxWidth="xl" sx={styles.container}>
+        <Link to="/home">
+          <Logo sx={styles.logo} />
+        </Link>
+
+        <Box sx={styles.btnsContainer}>
+          <Link to="/addproduct">
+            <Button sx={styles.btn}>
+              <PlusIcon sx={styles.plusIcon} />
+              <Typography sx={styles.addProductText}>Ürün Ekle</Typography>
+            </Button>
+          </Link>
+
+          <Link to={'/account'}>
+            <Button sx={styles.btn} startIcon={<PersonIcon />}>
+              Hesabım
+            </Button>
+          </Link>
+        </Box>
+      </Container>
     </AppBar>
-  )
-}
+  );
+};
