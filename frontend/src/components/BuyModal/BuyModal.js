784864f7bd235cd5435ee6a49b74b6c4bd5a6ae0
Egemen Noyan Seçkin <egemennoyan@hotmail.com>
2024-07-12 20:46:45 +0300

    RF-2 improve code in BuyModal

--- frontend/src/components/BuyModal/BuyModal.js
+++ frontend/src/components/BuyModal/BuyModal.js
@@ -1,53 +1,26 @@
 import * as React from 'react';
-
-import axios from 'axios'
-
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { styles } from './StylesBuyModal';
 
-import { styles } from './StylesBuyModal'
-
-export default function BuyModal({ isBuyModal, setIsBuyModal, productId, setProduct, token, setIsProductBought }) {
-
-  const handleClose = () => setIsBuyModal(false);
-
-  function handlePurchase(responseData) {
-    // setProduct belongs to detail page
-    if (setProduct) {
-      setProduct(responseData);
-    }
-    // detail and account pages
-    setIsProductBought(true);
-  }
-  async function buyProduct() {
-
-    const address = `https://bootcamp.akbolat.net/products/${productId}`
-    await axios.put(address, { isSold: true }, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((response) => handlePurchase(response.data)).catch((err) => console.log(err.message))
-
-    handleClose();
-  }
-
+export const BuyModal = ({ isBuyModal, onClose, onBuyProduct }) => {
   return (
     <Box>
       <Modal
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={isBuyModal}
-        onClose={handleClose}
+        onClose={onClose}
         closeAfterTransition
         disableAutoFocus
         BackdropComponent={Backdrop}
         BackdropProps={{
           timeout: 500,
-          style: { opacity: 0.7, background: '#4B9CE2' }
+          style: { opacity: 0.7, background: '#4B9CE2' },
         }}
       >
         <Fade in={isBuyModal}>
@@ -58,11 +31,15 @@ export default function BuyModal({ isBuyModal, setIsBuyModal, productId, setProd
             <Typography id="transition-modal-description" sx={styles.modalText}>
               Satın AImak istiyor musunuz?
             </Typography>
-            <Button sx={{...styles.button, ...styles.cancelButton}} onClick={handleClose}>Vazgeç</Button>
-            <Button onClick={buyProduct} sx={{...styles.button, ...styles.proceedButton}}>Satın AI</Button>
+            <Button sx={{ ...styles.button, ...styles.cancelButton }} onClick={onClose}>
+              Vazgeç
+            </Button>
+            <Button onClick={onBuyProduct} sx={{ ...styles.button, ...styles.proceedButton }}>
+              Satın AI
+            </Button>
           </Box>
         </Fade>
       </Modal>
     </Box>
   );
-}
+};
